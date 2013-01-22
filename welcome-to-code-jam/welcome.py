a22e656bfb0e2951383995ede90d05fa95900b94
wmgaca <wmgaca@gmail.com>
2013-01-22 16:24:38 +0100

    Qualification round 2009
    * Welcome to code jam

--- welcome-to-code-jam/welcome.py
+++ welcome-to-code-jam/welcome.py
@@ -1,6 +1,6 @@
 import sys
 
-count = 0
+values = {}
 
 def quick_test(needle, haystack):
     for c in needle:
@@ -17,48 +17,46 @@ def test(needle, haystack):
     return 0 == len(needle)
 
 def search(needle, haystack):
-    print '%s -> %s' % (needle, haystack)
-
+    if (needle, haystack) in values:
+        return values[(needle, haystack)]
+    
     if len(needle) > len(haystack):
         return 0
     if not needle and not haystack:
         return 0
     if len(needle) == 1:
-        print '  haystack.count(needle) = %s' % haystack.count(needle)
         return haystack.count(needle)
     if len(needle) == len(haystack):
         return 1 if needle == haystack else 0
     if needle == haystack:
         return 1
-
     if not quick_test(needle, haystack):
         return 0
     if not test(needle, haystack):
         return 0
 
     count = 0
-
     if len(needle) > 1:
         for index, h in enumerate(haystack):
             if h == needle[0]:
-                if 1 == len(needle[1:]):
-                    count += haystack[index + 1:].count(needle[1:])
-                    print "+ cs"
-                else:
-                    count += search(needle[1:], haystack[index + 1:])
-    
+                count += search(needle[1:], haystack[index + 1:])
+                count %= 1000
+
+    values[(needle, haystack)] = count
+
     return count
     
 def main():
     lines = [line.strip() for line in sys.stdin.readlines()[1:]]
 
+    global values
     needle = "welcome to code jam"
 
-    for index, line in enumerate(lines[:1]):
-        print line
+    for index, line in enumerate(lines):
+        values = {}
+
         line = ''.join([c for c in line if c in needle])
-        print line
-        print "Case #%s: %.4d" % (index + 1, search(needle, line))
+        print "Case #%s: %.4d" % (index + 1, search(needle, line) % 1000)
 
 if __name__ == '__main__':
     main()
