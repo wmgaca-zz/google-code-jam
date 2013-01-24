250a355b9cc341dd12c7ea65d6d40a1600af8a81
wmgaca <wmgaca@gmail.com>
2013-01-24 23:28:51 +0100

    Twibet.

---
+++ twibet/twibet.py
@@ -0,0 +1,35 @@
+import sys
+import collections
+
+def stdreadline(): return sys.stdin.readline().strip()
+def stdreadint(): return int(stdreadline())
+def stdreadints(): return [int(x.strip()) for x in stdreadline().split()]
+
+def process(start, monks):
+    whispered, heard = set(), set()
+    whispering = set([start])
+    while True:
+        if not whispering: break
+        for w in whispering:
+            heard.add(w)
+            whispered.add(w)
+            if w in monks:
+                heard = heard.union(monks[w])
+                whispering = whispering.union(monks[w])
+        whispering -= whispered
+    return len(whispered)
+
+T = stdreadint()
+for t in xrange(1, T+1):
+    N = stdreadint()
+    
+    followers = collections.defaultdict(set)
+
+    for f, val in enumerate(stdreadints(), start=1):
+        followers[val].add(f)
+    
+    print 'Case #%d:' % (t)
+    #print followers
+    for day in xrange(1, N+1):
+        print process(day, followers)
+        
